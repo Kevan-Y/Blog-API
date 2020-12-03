29d4dc41583337313714ded451e6c398b62b9ce4
Pandanox <58233223+Pandanoxes@users.noreply.github.com>
2020-12-02 21:01:58 -0500

    Formatting fix

--- modules/post-schema.js
+++ modules/post-schema.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 let postSchema = new Schema({
-    title: String,
-    postDate: Date,
-    featuredImage: String,
-    post: String,
-    postedBy: String,
-    comments: [{ author: String, comment: String, date: Date }],
-    category: String,
-    tags: [String],
-    isPrivate: Boolean,
-    views: Number
+  title: String,
+  postDate: Date,
+  featuredImage: String,
+  post: String,
+  postedBy: String,
+  comments: [{ author: String, comment: String, date: Date }],
+  category: String,
+  tags: [String],
+  isPrivate: Boolean,
+  views: Number,
 });
 
 module.exports = postSchema;
